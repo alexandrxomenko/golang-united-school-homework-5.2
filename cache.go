ad079bf5d3ee0c233789a9be4eb9fe25125796bb
Alexandr <alexandr.xomenko@gmail.com>
2022-05-23 12:19:44 +0300

    feat: Add code to Metods Get, Put, Keys, PutTill

--- cache.go
+++ cache.go
@@ -3,21 +3,35 @@ package cache
 import "time"
 
 type Cache struct {
+	KeyValue map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{make(map[string]string)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	value, exists := c.KeyValue[key]
+	return value, exists
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.KeyValue[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var sliceKey []string
+	for key := range c.KeyValue {
+		sliceKey = append(sliceKey, key)
+	}
+	return sliceKey
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.KeyValue[key] = value
+	go func() {
+		time.Sleep(deadline.Sub(time.Now()))
+		delete(c.KeyValue, key)
+	}()
+
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexandrxomenko/golang-united-school-homework-5.2/tree/master
+
+go 1.17
